998756bc3a50df45cfdc36069b6f24dae2a52c38
Dario Busco <dario.x.busco@gmail.com>
2026-07-16 20:16:38 +0200

    added latest point tile

--- public/app.js
+++ public/app.js
@@ -8,6 +8,37 @@ L.tileLayer(
     }
 ).addTo(map);
 
+const info = L.control({ position: "topright" });
+
+info.onAdd = function () {
+    this._div = L.DomUtil.create("div", "gps-info");
+    this.update();
+    return this._div;
+};
+
+info.update = function (p) {
+    if (!p) {
+        this._div.innerHTML = "<b>Latest GPS</b><br>No data";
+        return;
+    }
+
+    this._div.innerHTML = `
+        <h4>Latest GPS</h4>
+        <table>
+            <tr><td>Lat</td><td>${p.lat.toFixed(6)}</td></tr>
+            <tr><td>Lon</td><td>${p.lon.toFixed(6)}</td></tr>
+            <tr><td>Speed</td><td>${p.speed ?? "-"} km/h</td></tr>
+            <tr><td>Course</td><td>${p.course ?? "-"}°</td></tr>
+            <tr><td>Altitude</td><td>${p.altitude ?? "-"} m</td></tr>
+            <tr><td>Sats</td><td>${p.sats ?? "-"}</td></tr>
+            <tr><td>Time</td><td>${p.timestamp}</td></tr>
+        </table>
+    `;
+};
+
+info.addTo(map);
+
+
 let marker = null;
 let polyline = null;
 
@@ -38,9 +69,9 @@ async function refresh() {
         .addTo(map)
         .bindPopup("Latest position");
 
-    // map.fitBounds(polyline.getBounds(), {
-    //     padding: [40,40]
-    // });
+    const latest = points[points.length - 1];
+    info.update(latest);
+
 }
 
 refresh();
